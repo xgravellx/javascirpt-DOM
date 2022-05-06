6d00b2fab0dc1dfd4dcfa18d5daf0bd63ab65c9e
Hazal <88984994+xgravellx@users.noreply.github.com>
2022-05-06 18:04:34 +0300

    Delete getElementsByClassName.js

--- getElementsByClassName.js
+++
@@ -1,8 +0,0 @@
-// Ex-1
-let sonucc = document.getElementsByClassName("iki");
-for ( let i = 0; i < sonucc.length; i++) {
-    sonucc[i].style.backgroundColor = "#202020";
-    sonucc[i].style.color = "#FFFFFF";
-}
-
-
